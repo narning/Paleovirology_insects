206af3cb3d63000dc5ffa8f20301e7f4e1de2b82
Nicolas Arning <35806355+narning@users.noreply.github.com>
2018-01-29 11:50:07 +0000

    Delete go_into_every_folder_and_paste_folder_name_into_fasta_header.sh

--- go_into_every_folder_and_paste_folder_name_into_fasta_header.sh
+++
@@ -1,8 +0,0 @@
-for i in *_*
-do
-    cd $i
-    currentdir=$(basename `pwd`)
-    echo $currentdir
-    sed "s/^>/>${currentdir} /g" *cds*>${currentdir}_with_speciesnameinheader_cds.fa
-    cd /home/nick/uni/rotation_paleovirology_2018/dataset_collection/virus
-done
